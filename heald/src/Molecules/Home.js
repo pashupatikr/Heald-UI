4c66eb3f3d39a744e71a2e6607fefd5214b28fed
pashupati kumar <pashupatikumar04@gmail.com>
2021-04-30 23:45:20 +0530

    feat: add card for product

--- heald/src/Molecules/Home.js
+++ heald/src/Molecules/Home.js
@@ -1,25 +1,45 @@
+import zIndex from '@material-ui/core/styles/zIndex'
 import React from 'react'
 import CheckBoxWithText from '../Tools/CheckBoxWithText'
 import FiveStarRating from '../Tools/FiveStarRating'
 import PrimaryText from '../Tools/PrimaryText'
 import RadioButtonWithText from '../Tools/RadioButtonWithText'
 import SecondaryText from '../Tools/SecondaryText'
+import ProductCard from '../Widgets/Card/ProductCard'
 import MainHeader from '../Widgets/MainHeader'
+import SlideShow from '../Widgets/SlideShow'
 import SubHeader from '../Widgets/SubHeader'
 import Title from '../Widgets/Title'
 
 
 export default function Home() {
+ 
     return (
         <div>
           <MainHeader></MainHeader>
           <SubHeader></SubHeader>
-          <Title titletext="Heald"> </Title>
+          <SlideShow></SlideShow>
+
+          <div  style={{
+            border: "2px solid red", 
+            marginTop:"-440px",
+            zIndex:"220022",
+            position:"absolute",
+            height:"100%",
+            width:"99%"
+            
+          }}>
+            <ProductCard  ></ProductCard>
+            <ProductCard  ></ProductCard>
+            <ProductCard  ></ProductCard>
+            <ProductCard  ></ProductCard>
+          {/* <Title titletext="Heald"> </Title>
           <PrimaryText data="Multi Range" ></PrimaryText>   
           <SecondaryText data="Multi Range" ></SecondaryText>   
           <RadioButtonWithText data="Multi Range" ></RadioButtonWithText> 
           <CheckBoxWithText data="Get it in 2 days"></CheckBoxWithText> 
-          <FiveStarRating></FiveStarRating> 
+          <FiveStarRating></FiveStarRating>  */}
+          </div>
         </div>
     )
 }
---
+++ heald/src/Widgets/SlideShow.js
@@ -0,0 +1,43 @@
+import React from "react";
+import img1 from "../Elements/Image/SlideshowImg/1.jpg";
+import img2 from "../Elements/Image/SlideshowImg/2.jpg";
+import img3 from "../Elements/Image/SlideshowImg/3.jpg";
+
+export default function SlideShow() {
+  return (
+    <div>
+      <div
+        className="uk-position-relative uk-visible-toggle uk-light"
+        tabIndex="-1"
+        uk-slideshow="true"
+      >
+        <ul className="uk-slideshow-items">
+          <li>
+            <img src={img1} alt="" uk-cover="true" />
+          </li>
+          <li>
+            <img src={img2} alt="" uk-cover="true" />
+          </li>
+          <li>
+            <img src={img3} alt="" uk-cover="true" />
+          </li>
+        </ul>
+
+        <a
+          style={{  top: "150px"}}
+          className="uk-position-center-left uk-position-small uk-hidden-hover"
+          href="#"
+          uk-slidenav-previous="true"
+          uk-slideshow-item="previous"
+        ></a>
+        <a
+          className="uk-position-center-right uk-position-small uk-hidden-hover"
+          style={{ top: "150px"}}
+          href="#"
+          uk-slidenav-next="true"
+          uk-slideshow-item="next"
+        ></a>
+      </div>
+    </div>
+  );
+}
